f8029a1ab96c90a21dd46c236c63abfb762ecb6d
Juan Manuel Gonzalez Calleros <jumagoca78@users.noreply.github.com>
2025-01-27 12:51:13 -0600

    Version con pruebas completas

--- 03 Primeras clases/src/App.java
+++ 03 Primeras clases/src/App.java
@@ -2,15 +2,24 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // Crear una cuenta con saldo inicial de 1000
-        Cuenta cuenta = new Cuenta(1, 1000);
+        Cuenta cuentaFred = new Cuenta(1, 1000);
+        Cuenta cuentaSam = new Cuenta(2, 21000);
+        Cuenta cuentaEva = new Cuenta(3, 12000);
+        Cuenta cuentaProfesor = new Cuenta(4, 200);
 
         // Depositar 500
-        cuenta.depositar(500);
+        cuentaFred.depositar(500);
 
         // Retirar 200
-        cuenta.retirar(200);
+        cuentaFred.retirar(200);
 
         // Consultar saldo
-        System.out.println("Saldo: " + cuenta.consultarSaldo());
+        System.out.println("Saldo Fred: " + cuentaFred.consultarSaldo());
+        System.out.println("Saldo Sam: " + cuentaSam.consultarSaldo());
+        System.out.println("Saldo Eva: " + cuentaEva.consultarSaldo());
+        System.out.println("Saldo Profesor: " + cuentaProfesor.consultarSaldo());
     }
 }
+
+
+
