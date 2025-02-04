2266ce1862caeda292794921a6a3c02634e2d8e3
Juan Manuel Gonzalez Calleros <jumagoca78@users.noreply.github.com>
2025-02-04 12:49:52 -0600

    codigos de las diapositivas

---
+++ 06 Proyectos de Java/src/NotasConIf.java
@@ -0,0 +1,28 @@
+/* 
+ * Programa que lee una nota y muestra la calificación correspondiente
+ * utilizando la estructura de control if-else-if
+ * 
+ */
+import java.util.Scanner;
+public class NotasConIf {
+    public static void main(String[] args) {
+        int nota;
+        String textoNota;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresar nota:");
+        textoNota = scanner.nextLine();
+        nota = Integer.parseInt(textoNota);
+
+        if (nota >= 90) 
+            System.out.println("A");
+        else if (nota >= 80) 
+                System.out.println("B");
+            else if (nota >= 70) 
+                    System.out.println("C");
+                else if (nota >= 60) 
+                        System.out.println("D");
+                     else 
+                        System.out.println("F");
+        scanner.close();// Cerrar el scanner para liberar recursos
+    }
+}
