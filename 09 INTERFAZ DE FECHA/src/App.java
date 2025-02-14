af9615ffac65c1664af78a82e8ff0ebe6b8df139
Juan Manuel Gonzalez Calleros <jumagoca78@users.noreply.github.com>
2025-02-13 22:48:09 -0600

    tests

--- 09 INTERFAZ DE FECHA/src/App.java
+++ 09 INTERFAZ DE FECHA/src/App.java
@@ -2,11 +2,18 @@ public class App {
     public static void main(String[] args) throws Exception {
         //Instanciar un objeto de la clase Date
         Date d = new Date();
+        System.out.println(d.toString());
+        
         Date soloDia = new Date (13);
+        System.out.println(soloDia.toString());
+
         Date soloDiaMes = new Date (13, 5);
+        System.out.println(soloDiaMes.toString());
+
         Date soloDiaMesAño = new Date (101, 85, 20021);
+        System.out.println(soloDiaMesAño.toString());
+
         Date copia = new Date (soloDiaMesAño);
-        //Imprimir el valor de la fecha        
         System.out.println(copia.toString());
     }
 }
